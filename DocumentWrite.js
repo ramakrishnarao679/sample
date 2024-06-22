4f1988c12e90bab22bb54a1510778935cf039eb6
ramakrishnarao679 <ramakrishnarao679@gmail.com>
2024-06-22 13:18:13 +0530

    files added

---
+++ DocumentWrite.js
@@ -0,0 +1,11 @@
+
+            document.write("Enter the name");
+            document.write("Enter Emial");
+            document.write("Enter the phone");
+            document.write("Enter address");
+        
+                document.writeln("Enter the name");
+                document.writeln("Enter Email");
+                document.writeln("Enter the phone");
+                document.writeln("Enter the address");
+    
